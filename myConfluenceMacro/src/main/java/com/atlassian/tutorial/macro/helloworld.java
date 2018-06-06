c482a3fb7598d43fc8a1bad3acb98283981a9808
Hy <hy.duc.nguyen@mgm-tp.com>
2018-06-06 13:08:40 +0700

    Generate skeleton plugin - for unit testing later

--- myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/helloworld.java
+++ myConfluenceMacro/src/main/java/com/atlassian/tutorial/macro/helloworld.java
@@ -1,3 +1,28 @@
 package com.atlassian.tutorial.macro;
 
-import 
+import com.atlassian.confluence.content.render.xhtml.ConversionContext;
+import com.atlassian.confluence.macro.Macro;
+import com.atlassian.confluence.macro.MacroExecutionException;
+
+import java.util.Map;
+
+public class helloworld implements Macro {
+	public String execute (Map<String, String> map, String s, ConversionContext conversionContext) throws
+		MacroExecutionException {
+		if(map.get("Name") != null) {
+			return ("<h1>Hello " + map.get("Name") + "!</h1>");
+		}
+		else
+		{
+			return "<h1>Hello world</h1";
+		}
+	}
+
+	public BodyType getBodyType() {
+		return BodyType.NONE;
+	}
+
+	public OutputType getOutputType() {
+		return OutputType.BLOCK;
+	}
+}
